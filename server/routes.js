b2020b22629f411471925606aa8672b4453d3220
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-25 00:56:12 -0600

    API REST

--- server/routes.js
+++ server/routes.js
@@ -2,11 +2,22 @@
 var express = require('express');
 var router = express.Router();
 
+const mensajes = [
+  {
+   _id: 'XXX',
+   user: 'spiderman',
+   mensaje: 'Hola Mundo'
+
+  }
+];
+
+//Get mensajes
+router.get('/', function(req, res){
+  //res.json('Obteniendo mensajes');
+  res.json(mensajes);
 
 
-
-
-
+});
 
 // Get mensajes
 router.get('/', function (req, res) {
