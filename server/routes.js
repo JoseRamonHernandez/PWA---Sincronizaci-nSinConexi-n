13737115f49c117e2d49a128b18383ab3277c0de
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2022-10-25 01:12:53 -0600

    consumir servicio REST

--- server/routes.js
+++ server/routes.js
@@ -2,29 +2,45 @@
 var express = require('express');
 var router = express.Router();
 
+
 const mensajes = [
   {
    _id: 'XXX',
    user: 'spiderman',
    mensaje: 'Hola Mundo'
-
   }
+  
 ];
-
 //Get mensajes
-router.get('/', function(req, res){
-  //res.json('Obteniendo mensajes');
+router.get('/', function(req, res)
+{
   res.json(mensajes);
+});
 
+//Post mensajes
+router.post('/', function(req, res){
+  const mensaje={
+    mensaje:req.body.user,
+    user: req.body.user
+  };
+
+  mensajes.push(mensajes);
+
+  console.log(mensajes);
+
+  res.json(
+    {
+    ok:true,
+    mensaje
+
+  });
 
 });
 
+module.exports=router;
 // Get mensajes
 router.get('/', function (req, res) {
   res.json('Obteniendo mensajes');
 });
-
-
-
 
 module.exports = router;
